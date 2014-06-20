030cb31a799212e6577011bbd1ae6a47a46cb2b6
DanielDowns <djd133@msstate.edu>
2014-06-20 15:59:37 -0500

    Video Game start
    Initial setup

---
+++ Renegades/cruiser_script.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cruiser_script : enemyDamageBase_script {
+
+	int speed = 5;
+	int rotateSpeed = 5;
+
+	GameObject anchor;
+
+	string orders; //go left, right, or straight
+	int duration; //how long to do orders
+	int timer; //how long you've been doing orders
+
+	int fighterReserves = 10;
+	int guardNumber = 0;
+	int patNumber = 0;
+	int launchLimiter = 500;
+
+	int moveMultiplier = 50;
+
+	bool flip = false; //need to flip
+	bool doneFlip = false;
+	float startAngle = -1000;
+	int burnTime = 1000;
+	int burnTimer;
+
+	int frame_counter = 0;
+
+	// Use this for initialization
+	void Start () {
+		anchor = GameObject.Find ("centerAnchor");
+		health = 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frame_counter++;
+
+		if(flip){
+			ivan();
+		}
+		else{
+			move ();
+		}
+
+		if(Vector3.Distance(anchor.transform.position, gameObject.transform.position) > 1000){
+			flip = true;
+		}
+		launchFighter ();
+
+	}
+
+	void move(){
+		//movement
+		timer--;
+		gameObject.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		if(timer > 0){
+			if(orders.Equals("left")){
+				transform.Rotate(new Vector3(0,0,1) * Time.deltaTime * rotateSpeed);
+			}
+			else if(orders.Equals("right")){
+				transform.Rotate(new Vector3(0,0,-1) * Time.deltaTime * rotateSpeed);
+			}
+		}
+		else{
+			genOrders ();
+		}
+
+	}
+
+	void genOrders(){
+		int val = Random.Range (0, 5); //randomize action
+		int min = 0;
+		int max = 0;
+		if(val <= 1){
+			orders = "straight";
+			min = 5;
+			max = 11;
+		}
+		else if(val == 3){
+			orders = "left";
+			min = 5;
+			max = 20;
+		}
+		else if(val == 4){
+			orders = "right";
+			min = 5;
+			max = 20;
+		}
+		timer = duration = moveMultiplier * Random.Range(min,max);
+	}
+
+	void ivan(){
+		if(startAngle == -1000){
+			burnTimer = burnTime;
+			startAngle = gameObject.transform.rotation.eulerAngles.z;
+		}
+		if(doneFlip != true){
+			transform.Rotate(new Vector3(0,0,1) * Time.deltaTime * rotateSpeed);
+		}
+		else{
+			burnTimer--;
+		}
+
+		float end = startAngle + 180;
+		if(end > 360){
+			end -= 360;
+		}
+		if(end - gameObject.transform.rotation.eulerAngles.z < 2){
+			doneFlip = true;
+		}
+		gameObject.transform.Translate (Vector3.up * Time.deltaTime * speed);
+		if(burnTimer <= 0){
+			startAngle = -1000;
+			flip = false;
+		}
+	}
+
+	void launchFighter(){
+		if(fighterReserves > 0){
+			if(frame_counter % launchLimiter == 0){
+
+				if(guardNumber + patNumber < 5){
+					GameObject fighter = (GameObject)Instantiate(Resources.Load("Prefabs/enemyFighter"), gameObject.transform.position, gameObject.transform.rotation);
+					fighter.SendMessage("setOrders", "guard");
+					fighter.SendMessage("setMother", gameObject);
+				}
+
+			}
+		}
+	}
+}
---
+++ Renegades/enemyFighter_script.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyFighter_script : enemyDamageBase_script {
+
+	int frame_count = 0;
+
+	string orders = "guard";
+	GameObject mother;
+
+	int speed = 40;
+	int rotateSpeed = 80;
+
+	int coverMax = 200;
+	int scanLimiter = 20;
+	int engageDistance = 150;
+	int fireAngle = 10;
+	int fireLimiter = 30;
+
+	bool done = true;
+
+	GameObject [] allTargets;
+	GameObject target;
+
+	Vector3 targetPos; //the location to flying to
+	float idealAngle;
+
+	// Use this for initialization
+	void Start () {
+		allTargets = GameObject.FindGameObjectsWithTag("Ally");
+		health = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frame_count++;
+		gameObject.transform.Translate (Vector3.up * Time.deltaTime * speed);
+
+
+		//check for targets
+		acquireTarget();
+		fire ();
+
+		//generate random position
+		if(target == null){
+			if(orders.Equals("guard")){
+				//pick out new cruiser
+				if(Vector3.Distance(targetPos, mother.transform.position) > coverMax){
+					genPos();
+				}
+				if (Vector3.Distance(gameObject.transform.position, targetPos) < 5){
+					genPos();
+				}
+			}
+			//determine if change in course is needed
+			idealAngle = determineAngle(targetPos);
+		}
+		else{
+			idealAngle = determineAngle(target.transform.position);
+		}
+
+		//angle left/right if needed
+		if(idealAngle < 0){
+			idealAngle += 360;
+		}
+		if(Mathf.Abs(idealAngle - gameObject.transform.rotation.eulerAngles.z) > 1){
+			if(target == null){
+				manuever(targetPos);
+			}
+			else{
+				manuever(target.transform.position);
+			}
+		}
+
+	}
+
+	//generate random location near mother ship
+	void genPos(){
+		float X = Random.Range (mother.transform.position.x - 20, mother.transform.position.x + 20);
+		float Y = Random.Range (mother.transform.position.y - 20, mother.transform.position.y + 20);
+		targetPos = new Vector3 (X, Y, 0);
+	}
+
+	//the angle to rotate to be pointing at the target
+	float determineAngle(Vector3 v){ 
+		
+		float adj = (gameObject.transform.position.y - v.y);
+		float opp = (gameObject.transform.position.x - v.x);
+		
+		float cot = Mathf.Atan(opp/adj) * Mathf.Rad2Deg;
+		
+		cot *= -1;
+		if(v.y > gameObject.transform.position.y){
+			cot-=180;
+		}
+		cot += 180;
+
+		return cot;
+	}
+
+	//moves left/right to better close on target
+	void manuever(Vector3 loc){  
+
+		float Z = 1;
+
+		float comp = gameObject.transform.rotation.eulerAngles.z; 
+		float old = comp;
+
+		bool sentinal = false;
+		if (comp < 180) {
+		
+			if((comp - 180) <= idealAngle && idealAngle <= comp){
+				Z *= -1; //right
+				sentinal = true;
+			}
+			else{
+				comp += 360;
+				if((comp - 180) <= idealAngle && idealAngle <= comp){
+					Z *= -1; //right
+				}
+			}
+
+		}
+		else{
+			if((comp - 180) <= idealAngle && idealAngle <= comp){
+				Z *= -1; //right
+				sentinal = true;
+			}
+		}
+
+		gameObject.transform.Rotate (new Vector3 (0, 0, Z) * Time.deltaTime * rotateSpeed);
+	}
+
+	//scans for all viable targets and attempts to pick one in range
+	int acquireTarget(){ 
+		if(frame_count % scanLimiter == 0){
+			allTargets = GameObject.FindGameObjectsWithTag("Ally");
+		}
+
+		foreach (GameObject t in allTargets){
+			if(t != null){
+				if(Vector3.Distance(gameObject.transform.position, t.transform.position) < engageDistance){
+					target = t;
+					return 0;
+				}
+			}
+		}
+		target = null;
+		return 0;
+	}
+
+	int fire(){
+		if(target == null){
+			return 0;
+		}
+
+		if(Mathf.Abs(idealAngle - gameObject.transform.rotation.eulerAngles.z) <= 10){
+			if(frame_count % fireLimiter == 0){
+				Instantiate(Resources.Load("Prefabs/enemyBullet"), gameObject.transform.position, gameObject.transform.rotation);
+			}
+		}
+		return 0;
+	}
+
+	public void setOrders(string s){
+		orders = s;
+	}
+
+	public void setMother(GameObject m){
+		mother = m;
+	}
+}
